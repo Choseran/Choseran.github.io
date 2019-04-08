6ff834b44f2d666c2305d3dd1c67951be472e239
seran <rldlfdj@naver.com>
2019-04-08 11:39:07 +0900

    up

---
+++ chuu/chuulove.js
@@ -0,0 +1,6 @@
+ window.onload=function(){
+   $("#last").append("<figure>
+     <a href="https://twitter.com/WS_yves/status/1112706042956136448" target="blank"><img src="cal_1.png" >
+     <figcaption><p><b>190316 Photographer Orbit Event</b></p><h6>© WS_yves</h6></figcaption></a>
+   </figure>");
+ }
