2e27cc690d046bed21457810f870b03a9356d9b4
seran <rldlfdj@naver.com>
2019-04-08 17:19:21 +0900

    plus

---
+++ chuu/chuulove.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    console.log('작동됩니다');
+    $("#more").click(function(){
+      $(".hide").show(500);
+      $(".hide").css('display','inline-block');
+    });
+  });
