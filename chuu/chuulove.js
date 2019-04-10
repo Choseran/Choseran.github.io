b5d20b1fbb9edca309b0dfc68fca512919bec1fd
seran <rldlfdj@naver.com>
2019-04-10 15:07:07 +0900

    delete

--- chuu/chuulove.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    console.log('작동됩니다');
-    $("#more").click(function(){
-      $(".hide").show(500);
-      $(".hide").css('display','inline-block');
-    });
-  });
