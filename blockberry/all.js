a432fa2a67b9acb85d3834cc3fa82b36eb23cab4
seran <rldlfdj@naver.com>
2019-04-29 19:52:24 +0900

    RealRealLastEdit_190429

--- blockberry/all.js
+++ blockberry/all.js
@@ -12,19 +12,24 @@ $(document).ready(function(){
         $("html").stop().animate({scrollTop:position.top},500);
         console.log(position);
       });
-      $(sub_bt[1]).click(function() {
-        position = $("#one").offset();
-        $("html").stop().animate({scrollTop:position.top},500);
-        console.log(position);
-      });
-      $(sub_bt[2]).click(function() {
-        position = $("#last").offset();
-        $("html").stop().animate({scrollTop:position.top},500);
-        console.log(position);
-      });
-      $(sub_bt[3]).click(function() {
-        position = $("footer").offset();
-        $("html").stop().animate({scrollTop:position.top},500);
-        console.log(position);
-      });
+    $(sub_bt[1]).click(function() {
+      position = $("#one").offset();
+      $("html").stop().animate({scrollTop:position.top},500);
+      console.log(position);
+    });
+    $(sub_bt[2]).click(function() {
+      position = $("#last").offset();
+      $("html").stop().animate({scrollTop:position.top},500);
+      console.log(position);
+    });
+    $(sub_bt[3]).click(function() {
+      position = $("footer").offset();
+      $("html").stop().animate({scrollTop:position.top},500);
+      console.log(position);
+    });
+    $(function() {
+      $('#sub_menu').click(function() {
+        $('#sub_menu>span').toggleClass('on')
+      })
+    });
   });
